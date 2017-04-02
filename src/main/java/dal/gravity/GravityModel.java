94db0c6a9214a53ad9183f6f7b102e5680adfc00
Liam Gowan <liam@dal.ca>
2017-04-02 19:40:43 -0300

    Updated PendulumRunner and GravityModel for Jupiter.
    Added some of requirements for this step in step 2.

--- src/main/java/dal/gravity/GravityModel.java
+++ src/main/java/dal/gravity/GravityModel.java
@@ -2,5 +2,6 @@ package dal.gravity;
 
 public interface GravityModel {
 	public static double EARTHGRAVITY = 9.80655;
+	public static double JUPITERGRAVITY = 25;
 	public double getGravitationalField();
 }
--- src/main/java/dal/gravity/PendulumRunner.java
+++ src/main/java/dal/gravity/PendulumRunner.java
@@ -10,9 +10,6 @@ public class PendulumRunner {
 
     public static void main (String [] args) {
     	GravityModel earthGrav = new GravityConstant(GravityModel.EARTHGRAVITY);
-    	NumberFormat nf = NumberFormat.getInstance ();
-    	nf.setMaximumFractionDigits (3);
-
     	double delta = (args.length == 0) ? .1 : Double.parseDouble (args[0]);
     	double sLen = 10, pMass = 10, theta0 = Math.PI/30;
     	RegularPendulum rp = new RegularPendulum (sLen, pMass, theta0, delta,earthGrav );
@@ -22,9 +19,26 @@ public class PendulumRunner {
 
     	// print out difference in displacement in 1 second intervals
     	// for 20 seconds
+    	printPendulumReadings(rp,sp,rpCoarse,delta,1,20);
+    	
+    	GravityModel jupGrav = new GravityConstant(GravityModel.JUPITERGRAVITY);
+    	
+    	rp.changeGravitationalConstant(jupGrav);
+    	sp.changeGravitationalConstant(jupGrav);
+    	rpCoarse.changeGravitationalConstant(jupGrav);
+    	
+    	System.out.println("\n\nSwitching to model Jupiter's Gravity\n\n");
+    	
+    	printPendulumReadings(rp,sp,rpCoarse,delta, 21,40);
+    }
+    
+    //Prints all readings to make it more efficient. Takes objects, delta and start and end time.
+    public static void printPendulumReadings(RegularPendulum rp, SimplePendulum sp, RegularPendulum rpCoarse, double delta, int start, int end){
     	int iterations = (int) (1/delta);
+    	NumberFormat nf = NumberFormat.getInstance ();
+    	nf.setMaximumFractionDigits (3);
     	System.out.println ("analytical vs. numerical displacement (fine, coarse)");
-    	for (int second = 1; second <= 20; second++) {
+    	for (int second = start; second <= end; second++) {
     		for (int i = 0; i < iterations; i++) rp.step ();
     		for (int i = 0; i < 10; i++) rpCoarse.step (); 
 	    	System.out.println ("t=" + second + "s: \t" + 
@@ -36,4 +50,6 @@ public class PendulumRunner {
     	}
     }
 }
+
+
 
